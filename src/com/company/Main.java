1656471eb54c67d053c67bc313e215f731bbdebb
Holly Chaffee <saltdc95@gmail.com>
2020-11-16 12:22:44 -0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Object[] list = {new Dog(), new DogFood()};
+	    for (Object object : list) {
+	        if (object instanceof Animal) {
+                ((Animal) object).makeSound();
+            }
+        }
+    }
+}
+
+class Animal {
+    String sound = "";
+
+    void makeSound() {
+        System.out.println(sound);
+    }
+}
+
+class Dog extends Animal {
+    Dog() {
+        sound = "bark";
+    }
+}
+
+class DogFood {}
